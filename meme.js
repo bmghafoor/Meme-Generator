18cc0e7523a14ba6e50d2380576673acafe4f531
Binyameen Ghafoor <bmghafoor@Binyameens-MBP.home>
2022-08-05 14:21:27 -0400

    Set char limit and reorg code

--- meme.js
+++ meme.js
@@ -7,67 +7,81 @@ const memeForm = document.querySelector('#memeForm')
 memeForm.addEventListener('submit', function(event){ 
 
     event.preventDefault() 
-
+    // div that will contain image,captions, and delete button
     const memeContent = document.createElement('div') 
-    const topCaption = document.createElement('div') 
-    const bottomCaption = document.createElement('div') 
-    const deleteButton = document.createElement('button') 
-    
-
     memeContent.classList.add('meme') 
 
-    const image = document.createElement('img') // create a new html element when page is submitted
+    // image
+    const image = document.createElement('img') 
+    image.classList.add('memeIMG') 
     if (imgUrl.value == ''){ // if there is no image input, throw an error
         alert('Image URL needed')
         try {
             throw new Error("Image url needed")
         } catch {
-            console.log('Image Url field is blank') 
             return
         }
     }
-
     function isValidUrl(url){
         try{
-            return Boolean(new URL(url));
+            return Boolean(new URL(url)); // creates new URL Object which will return true/false depending on whether url exists
         } catch{
             return false
         }
     }
-
     if(isValidUrl(imgUrl.value)) {image.src = imgUrl.value }
 
     else{
         alert('invalid Url')
-        console.log('Enter Valid Url')
         return
     }
 
-    
-    image.classList.add('memeIMG') 
-
+    // top caption
+    const topCaption = document.createElement('div') 
     topCaption.innerHTML = topText.value 
     topCaption.classList.add('topCaption') 
+    if(topText.value.length > 60){ 
+        try{
+            throw new Error('Top Text too long')
+        } catch {
+            alert('Top caption must be under 60 characters')
+            return
+        }
+    }
 
+
+    // bottom caption
+    const bottomCaption = document.createElement('div') 
     bottomCaption.innerHTML = bottomText.value 
     bottomCaption.classList.add('bottomCaption') 
+    if(bottomText.value.length > 60){
+        try{
+            throw new Error('Bottom Text too long')
+        } catch {
+            alert('Bottom caption must be under 60 characters')
+            return
+        }
+    }
 
+
+    // delete button
+    const deleteButton = document.createElement('button') 
     deleteButton.classList.add('deleteBtn') 
     deleteButton.innerHTML = 'Delete' 
-
-    document.body.appendChild(memeContent)  // Appending the container for the meme to our document body, otherwise it won't show up
-    memeContent.appendChild(image) // Add the image as a child to the meme container
-    memeContent.appendChild(topCaption)
-    memeContent.appendChild(bottomCaption) 
-    memeContent.appendChild(deleteButton)
-    topText.value = '' // After the form is submitted, the input box for Top caption will be cleared (set to an empty string)
-    bottomText.value = '' // After the form is submitted, the input box for Bottom caption will be cleared (set to an empty string)
-    url.value = '' // After the form is submitted, the input box for URL will be cleared (set to an empty string)
-
-    memeContent.addEventListener('click',function(event){ // Listens for a click on the meme container 
-        if(event.target.tagName ==='BUTTON'){ // If the element that was clicked on is a button
+    memeContent.addEventListener('click',function(event){ 
+        if(event.target.tagName ==='BUTTON'){ 
             event.target.parentElement.remove() // Remove the parent element (the meme container that contains the image,captions, and button pressed)
         }
     })
 
+
+    // Want to append everything last incase an error is caught above
+    document.body.appendChild(memeContent)  
+    memeContent.appendChild(image) 
+    memeContent.appendChild(topCaption)
+    memeContent.appendChild(bottomCaption) 
+    memeContent.appendChild(deleteButton)
+    topText.value = '' 
+    bottomText.value = '' 
+    url.value = '' 
 })
